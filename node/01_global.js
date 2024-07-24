1d2e6495d4b51919705e6eef05a76712b0698046
Dagim <dagimgetaW27@gmail.com>
2024-07-24 21:57:07 +0300

    global

---
+++ node/01_global.js
@@ -0,0 +1,8 @@
+// __dirname => display current working directory
+// __filename => display file name
+// require => function to use module
+// module => info about current module
+// process => info about env where the program is being executed
+
+console.log(__dirname);
+console.log(__filename);
